5ecb8898b3cfc4b5de5d6a57c840453d02d4c75a
tomo536 <tomo536@gmail.com>
2009-11-04 00:10:17 +0900

    Closes #4 add validate for book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,15 @@
 class Book < ActiveRecord::Base
+  validates_presence_of :title
+  validates_length_of :title, :maximum => 256
+
+  validates_presence_of :author
+  validates_length_of :author, :maximum => 256
+
+  validates_presence_of :publisher
+  validates_length_of :publisher, :maximum => 256
+
+  validates_presence_of :pubdate
+
+  validates_presence_of :price
+  validates_numericality_of :price, :greater_than_or_equal_to => 0
 end
