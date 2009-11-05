36e8ef9acbe5a4ca1707aa421c79c6df7a084175
tomo536 <tomo536@gmail.com>
2009-11-06 01:31:59 +0900

    fixed #5

--- app/models/book.rb
+++ app/models/book.rb
@@ -12,4 +12,8 @@ class Book < ActiveRecord::Base
 
   validates_presence_of :price
   validates_numericality_of :price, :greater_than_or_equal_to => 0
+  
+  def price=(value)
+    self[:price] = value.gsub(/,/, "")
+  end
 end
